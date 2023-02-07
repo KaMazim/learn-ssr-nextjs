f042f4232506b45857c56a757f40b1d5bed0d5c8
kamazim <kainamazim@gmail.com>
2023-02-07 03:34:43 -0300

    feat: resources and template code added

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-export default () => <p>Hello, World!</p>;
+import Card from './Card';
+
+import styles from './index.module.css';
+
+export default () => {
+    return (
+        <div className={styles.app}>
+            <header className={styles.header}>
+                <img src='/logo.png' className={styles.logo} alt='logo' />
+            </header>
+            <div className={styles.grid}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
+};
