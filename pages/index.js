818093d98704be859ef9bcf5d4d3190e5e9273ec
kamazim <kainamazim@gmail.com>
2023-02-07 03:47:54 -0300

    feat: data fetching added

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
+import data from './api/data.json';
+
 import Card from './Card';
 
 import styles from './index.module.css';
 
-export default () => {
+const Index = ({ cards }) => {
     return (
         <div className={styles.app}>
             <header className={styles.header}>
                 <img src='/logo.png' className={styles.logo} alt='logo' />
             </header>
             <div className={styles.grid}>
-                <Card />
-                <Card />
-                <Card />
+                {cards.map((card) => (
+                    <Card key={card.id} />
+                ))}
             </div>
         </div>
     );
 };
+
+Index.getInitialProps = async () => {
+    return { cards: data };
+};
+
+export default Index;
